aff74a714c4488cd0a46034ab7ee9f7b4d697164
Usarneme <jamestcoop@gmail.com>
2021-05-28 08:27:37 -0700

    :building_construction: builds out controllers and index views for engineers and machines

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+  }
+}
