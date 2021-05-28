c5330e50277b43d0b63d8c2b906c8786f021f4eb
Usarneme <jamestcoop@gmail.com>
2021-05-28 08:41:27 -0700

    :monocle_face: updates factorycontext to include references needed for join entities and lazy loading

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -4,6 +4,13 @@ namespace Factory.Models
 {
   public class FactoryContext : DbContext
   {
-
+    public FactoryContext(DbContextOptions options) : base(options) {}
+    public virtual DbSet<Engineer> Engineers { get; set; }
+    public virtual DbSet<Machine> Machines { get; set; }
+    public DbSet<EngineerMachine> EngineerMachines { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
