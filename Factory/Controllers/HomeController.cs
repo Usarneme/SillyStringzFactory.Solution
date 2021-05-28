6f1819ac713e18457402a15a3bb95e0a7444a23c
Usarneme <jamestcoop@gmail.com>
2021-05-28 14:39:41 -0700

    :tada: updates home controller and view to display all engineers and machines

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -10,8 +10,16 @@ namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
+      ViewBag.Engineers = _db.Engineers.ToList();
+      ViewBag.Machines = _db.Machines.ToList();
       return View();
     }
 
