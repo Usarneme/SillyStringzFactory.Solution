a122b0094eb4daa1782ca2f75a41329300b72264
Usarneme <jamestcoop@gmail.com>
2021-05-28 08:35:27 -0700

    :monocle_face: updates models for engineer and machine

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Factory.Models
 {
   public class Engineer
   {
-    
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
   }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Factory.Models
 {
   public class Machine
   {
-    
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
   }
 }
