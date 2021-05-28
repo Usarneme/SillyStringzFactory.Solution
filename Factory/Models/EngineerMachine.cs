3155d899147dbbe32841b305fc16550eee1556d2
Usarneme <jamestcoop@gmail.com>
2021-05-28 08:37:53 -0700

    :monocle_face: updates engineermachine join entity class model

--- Factory/Models/EngineerMachine.cs
+++ Factory/Models/EngineerMachine.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Factory.Models
 {
   public class EngineerMachine
   {
-    
+    public int EngineerMachineId { get; set; }
+    public int EngineerId { get; set; }
+    public int MachineId { get; set; }
+    public virtual Engineer Engineer { get; set; }
+    public virtual Machine Machine { get; set; }
   }
 }
